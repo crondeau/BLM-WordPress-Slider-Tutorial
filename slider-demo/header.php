d518837fcbdb130542b95cd0f5941bcb7e6c459a
Christine Rondeau <christine@bluelimemedia.com>
2012-03-01 16:58:18 -0800

    Update to JQuery
    I inserted the path to the image which will preload the loading image.
    I also updated the css with the link to the tutorial

--- slider-demo/header.php
+++ slider-demo/header.php
@@ -14,7 +14,7 @@
 	jQuery(document).ready(function($){ 
 		$('#slides').slides({
 			preload: true,
-			preloadImage: 'images/loading.gif', //you'll need to insert the full  path here
+			preloadImage: '<?php echo get_template_directory_uri(); ?>/images/loading.gif',
 			play: 5000, // change this to make it go faster or slower
 			pause: 2500, //change this to pause more or less
 			hoverPause: true
